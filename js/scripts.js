a3ad40558a52c5eb4c8adccfd9e1177bdafed905
Svea Wade <svealinneawade@gmail.com>
2020-10-28 11:41:22 -0700

    Passed 4th Test
    Co-authored-by: Chelsea Becker <cschweig2@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,9 @@
 
 //User Interface Logic//
 function isLeapYear(year) {
-  if (year % 100 === 0) {
-    return false;
-  } else if (year % 4 === 0) { 
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) { 
     return true;
-  } else {
+  }else {
     return false;
   }
 }
--- js/tests/scriptsTest.js
+++ js/tests/scriptsTest.js
@@ -1,10 +1,17 @@
 // Describe: isLeapYear()
 
+//Test #1
 //Test: "It returns false for years that are not a leap year"
 //Expect(isLeapYear(1993)).toEqual(false);
 
+//Test #2
 //Test: "It returns true for years that are divisible by 4"
 //Expect (isLeapYear(2004)).toEqual(true);
 
+//Test #3
 //Test: "It will return false for years that are divisible by 100"
 //Expect(isLeapYear(2100)).toEqual(false);
+
+//Test #4
+//Test: "It will return true for years that are divisible by 400"
+//Expect (isLeapYear(2000)).toEqual(true);
