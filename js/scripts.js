78e331ac8032caa2b9a17d6949cad62ea4aeb2b8
Svea Wade <svealinneawade@gmail.com>
2020-10-28 13:20:28 -0700

    final commit
    Co-authored-by: Chelsea Becker <cschweig2@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,15 @@
 //Business Logic//
 
 //User Interface Logic//
-function isLeapYear(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) { 
-    return true;
-  }else {
-    return false;
-  }
-}
+$(document).ready(function() {
+  $("#form").submit(function(event){
+    event.preventDefault();
+    const year = $("input#year").val();
+
+    if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) { 
+    alert("This is a leap year!!");
+    } else {
+    alert("This is not a leap year :(");
+    }
+  });
+});
